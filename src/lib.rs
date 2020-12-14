109a50d3b8996b1f0135e3c71d5881ce971e4290
dollarkiller <adapawang@gmail.com>
2020-12-14 11:08:13 +0800

    [ADD] more test

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,5 @@ mod error;
 mod http;
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Sync + Send>>;
+
+pub use async_trait::async_trait;
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ use tide::{
     Request,
     prelude::*,
     Endpoint,
-
+    Response,
+    Result,
 };
+
+use storm::*;
 
 #[derive(Debug, Deserialize)]
 struct Animal {
@@ -15,14 +18,21 @@ struct Animal {
 async fn main() -> tide::Result<()> {
     let mut app = tide::new();
 
-    // let core = Core {};
-    // app.at("/orders/shoes").post(p.order_shoes);
-    app.at("/orders/shoes").post(Core::order_shoes);
-    app.listen("127.0.0.1:8080").await?;
+    let core = Core {};
+    app.at("/orders/shoes").get(core);
+    app.at("/orders/shoes2").get(Core::order_shoes);
+    app.listen("127.0.0.1:8181").await?;
     Ok(())
 }
 
 struct Core {}
+
+#[async_trait]
+impl<State: Clone + Send + Sync + 'static> Endpoint<State> for Core {
+    async fn call(&self, req: Request<State>) -> Result<Response> {
+        Ok(format!("Hello World").into())
+    }
+}
 
 impl Core {
     async fn order_shoes(mut req: Request<()>) -> tide::Result {
@@ -35,6 +45,14 @@ impl Core {
     //         let Animal { name, legs } = req.body_json().await?;
     //         Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
     //     }
+    // }
+
+    // async fn order_shoes_test2(&self) -> Box<dyn Future<Output=tide::Result>> {
+    //     // 我想用self的一些变量
+    //     Box::new(async move |mut req: Request<()>| -> tide::Result {
+    //         let Animal { name, legs } = req.body_json().await?;
+    //         Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
+    //     })
     // }
 }
 
