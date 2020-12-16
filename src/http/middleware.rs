c0fc66e7cffe2c3a3f830e3a1a2f2b85be829f4c
dollarkiller <adapawang@gmail.com>
2020-12-16 19:30:43 +0800

    UDP Context

--- src/http/middleware.rs
+++ src/http/middleware.rs
@@ -5,19 +5,19 @@ use async_std::future::Future;
 
 /// Middleware handle function
 #[async_trait]
-pub trait Middleware<State: Clone + Send + Sync + 'static, V: Debug> {
+pub trait Middleware<State: Clone + Send + Sync + 'static> {
     /// 具体实现方法
-    async fn handle(&self, ctx: Context<State, V>);
+    async fn handle(&self, ctx: Context<State>);
 }
 
 #[async_trait]
-impl<State, F, Fut> Middleware<State,V> for F
+impl<State, F> Middleware<State> for F
     where
         State: Clone + Send + Sync + 'static,
-        F: Send + Sync + 'static + Fn(Context<State, V>) -> Fut,
+        F: Send + Sync + 'static + Fn(Context<State>),
 {
-    async fn handle(&self, ctx: Context<State, V>) {
+    async fn handle(&self, ctx: Context<State>) {
         let fut = (self)(ctx);
-        fut.await?;
+        fut.await;
     }
 }
